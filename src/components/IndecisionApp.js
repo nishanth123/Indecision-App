c0f2e3bfb581a94a305e9141a43924857c175c79
nishanth <nishanth@codeastra.com>
2018-01-04 08:46:07 +0530

    added code to clear 33% of edge-cases

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -17,7 +17,6 @@ export default class IndecisionApp extends React.Component {
 
 		this.totalOptions = [];
 		this.count = 0;
-		this.optionPoint = -1;
 
 		this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
 		this.handleClearSelectedOption = this.handleClearSelectedOption.bind(this);
@@ -28,53 +27,464 @@ export default class IndecisionApp extends React.Component {
 
 	handleDeleteOptions(){
 		this.count = 0;
-		this.optionPoint = -1;
-
-		console.log('handleDeleteOptions');
+		
 		this.setState(() => ({ options: [] }));
 	};
 
 	handleClearSelectedOption(){
 		this.setState(() => ({ selectedOption: undefined }));
 
-		this.optionPoint = -1;
 	}
 
 	handleDeleteOption(optionToRemove){
 		
 		this.count = this.count - 1;
-		//this.options.filter((option) => optionToRemove !== option);
-
-		console.log('Count is ' + this.count);
-
+		
 		var len = 0;
 
 		if (this.state.options !== undefined){
 			len = this.state.options.length;
 		}
+		console.log('Length is ' + len);
 
 		if (len > 2)
 		{
-			console.log('The number is: ' + (this.count - 3));
-
-			// len = 3. 
-			console.log(this.state.options);
-
-			console.log(this.totalOptions);
-
 			var newOption = this.totalOptions[this.count - 3];
 
-			console.log(newOption);
+			if (this.count > 2)
+			{	
+				
+				this.setState((prevState) => {
+				var n = prevState.options.indexOf(optionToRemove);
+					
+					if (n == 0)
+					{
+						
+						if (this.count > 3)
+						{
+							console.log(this.count);
+							prevState.options[0] = newOption;
+						}
+						else if (this.count > 2)
+						{
+							console.log(this.count);
+							prevState.options[0] = newOption;
+							console.log('test1');
 
-			this.setState((prevState) => ({ 
-				options: prevState.options.filter((option) => optionToRemove !== option).concat(newOption)
-			}));
+							//prevState.options[0] = prevState.options[1];
+							//prevState.options[1] = prevState.options[2];
+
+							//prevState.options[2] = undefined;
+							
+							//this.setState({ options: prevState.options.splice(2) });
+						}
+						else if (this.count > 1)
+						{
+							prevState.options[0] = prevState.options[1];
+							prevState.options[1] = prevState.options[2];
+							
+							console.log(this.count);
+							var array = prevState.options;
+							console.log('before logging1');
+							console.log(array);
+							array = array.slice(0, 1);
+							console.log('before logging2');
+							console.log(array);
+
+							this.setState({ options: array });
+						}
+						else if (this.count > 0)
+						{
+							//var array = prevState.options;
+							//array.splice(1);
+							console.log('2.');
+
+							prevState.options[2] = undefined;
+							
+							this.setState({ options: prevState.options.splice(0) });
+						}
+						else
+						{
+								//var array = prevState.options;
+								//array.splice(0);
+								console.log('3.');
+
+								this.setState({ options: [] });
+						} 
+					} // n == 0
+					else if (n == 1)
+					{
+						prevState.options[2] = prevState.options[2];
+						prevState.options[1] = prevState.options[0];
+
+						if (this.count > 3)
+						{
+							prevState.options[0] = newOption;
+						}
+						else
+						{
+							this.setState(() => ({ options: [] }));
+						}
+					}
+					else
+					{
+						prevState.options[2] = prevState.options[1];
+						prevState.options[1] = prevState.options[0];
+
+						if (this.count > 3)
+						{
+							prevState.options[0] = newOption;
+						}
+						else
+						{
+							this.setState(() => ({ options: [] }));
+						}
+					}
+
+					return {
+						options: prevState.options
+					}
+				})// setState end
+
+
+			} // this.count > 2
+			else
+			{
+				
+				if (this.count > 3)
+				{
+					console.log('test2 3');
+
+					prevState.options[0] = prevState.options[1];
+					prevState.options[1] = prevState.options[2];
+
+					prevState.options[2] = undefined;
+					//this.setState({ options: prevState.options.splice(2) });
+				}
+				else if (this.count > 2)
+				{
+					console.log('test2 2');
+				}
+				else if (this.count > 1)
+				{
+					//var n = prevState.options.indexOf(optionToRemove);
+
+					console.log('test2 1');
+
+					this.setState((prevState) => {
+					
+						console.log('test2 1 0');
+						//this.setState({ options: [] });
+						this.setState({ options: prevState.options.splice(1) });
+
+						console.log('test2 1 0 1');
+						//prevState.options[0] = prevState.options[1];
+						//prevState.options[1] = prevState.options[2];						
+					});
+
+					console.log('test2 1 0 2');
+					//prevState.options[2] = undefined;
+				}
+				else
+				{
+					console.log('test2 0');
+
+					this.setState((prevState) => ({ 
+						options: prevState.options.filter((option) => optionToRemove !== option).concat(newOption)
+					}));
+				}
+			}
 			
+		}// len > 2		
+		else if (len > 1) {
+
+			console.log('test2 0 start 1');
+
+			var newOption = this.totalOptions[this.count - 3];
+			
+			console.log('test2 0 start 2');
+			if (this.count > 2)
+			{	
+				console.log('test2 0 end 1 2');
+				
+				//this.setState((prevState) => ({ 
+				//	options: prevState.options.filter((option) => optionToRemove !== option)
+				//}));
+			}
+			else if (this.count > 1)
+			{
+				console.log('test2 0 end 1 1');
+			}
+			else if (this.count > 0)
+			{
+				console.log('test2 0 end 1 0');
+
+				this.setState((prevState) => {
+					var n = prevState.options.indexOf(optionToRemove);
+						
+					if (n == 0)
+					{
+						if (this.count > 3)
+						{
+							//prevState.options[0] = prevState.options[1];
+							console.log('$' + this.count);
+
+						}
+						else if (this.count > 2)
+						{
+							console.log('$' + this.count);
+						}
+						else if (this.count > 1)
+						{
+							
+							console.log('$' + this.count);
+							
+						}
+						else if (this.count > 0)
+						{
+							console.log('$' + this.count);
+							prevState.options[0] = prevState.options[1];
+							console.log('$1' + this.count);
+							this.setState({ options: prevState.options.splice(1) });
+							console.log('$2' + this.count);
+						}
+						else
+						{
+							console.log('$' + this.count);
+						} 
+					} // n == 0
+					else if (n == 1)
+					{
+						prevState.options[2] = prevState.options[2];
+						prevState.options[1] = prevState.options[0];
+
+						if (this.count > 3)
+						{
+							prevState.options[0] = newOption;
+						}
+						else
+						{
+							this.setState(() => ({ options: [] }));
+						}
+					}
+					else
+					{
+						prevState.options[2] = prevState.options[1];
+						prevState.options[1] = prevState.options[0];
+
+						if (this.count > 3)
+						{
+							prevState.options[0] = newOption;
+						}
+						else
+						{
+							this.setState(() => ({ options: [] }));
+						}
+					}
+	
+					return {
+						options: prevState.options
+					}
+				})//setState
+
+				//prevState.options[0] = prevState.options[1];
+				console.log('test2 0 end 1 -0.5');
+
+				//this.setState((prevState) => ({ 
+				//	options: prevState.options.filter((option) => optionToRemove !== option)
+				//}));
+			}
+			else 
+			{
+				console.log('test2 0 end 1 -1');
+			}
+
+			console.log('test2 0 end 3');
 		}
-		else {
-			this.setState((prevState) => ({ 
-				options: prevState.options.filter((option) => optionToRemove !== option)
-			}));
+		else if (len > 0) {
+			console.log('len0 0 start 1');
+			
+			var newOption = this.totalOptions[this.count - 3];
+						
+			console.log('len0 0 start 2 ' + this.count);
+			if (this.count > 2)
+			{	
+				console.log('len0 0 end 1 2');
+							
+				//this.setState((prevState) => ({ 
+				//	options: prevState.options.filter((option) => optionToRemove !== option)
+				//}));
+			}
+			else if (this.count > 1)
+			{
+				console.log('len0 0 end 1 1');
+			}
+			else if (this.count > 0)
+			{
+				console.log('len0 0 end 1 0');
+			
+				this.setState((prevState) => {
+				var n = prevState.options.indexOf(optionToRemove);
+									
+				if (n == 0)
+				{
+					if (this.count > 3)
+					{
+						//prevState.options[0] = prevState.options[1];
+						console.log('$len0' + this.count);
+			
+					}
+					else if (this.count > 2)
+					{
+						console.log('$len0' + this.count);
+					}
+					else if (this.count > 1)
+					{
+										
+						console.log('$len0' + this.count);
+										
+					}
+					else if (this.count > 0)
+					{
+						console.log('$len0' + this.count);
+						prevState.options[0] = prevState.options[1];
+						console.log('$len01' + this.count);
+						this.setState({ options: prevState.options.splice(1) });
+						console.log('$len02' + this.count);
+					}
+					else
+					{
+						console.log('$len0' + this.count);
+					} 
+				} // n == 0
+				else if (n == 1)
+				{
+					prevState.options[2] = prevState.options[2];
+					prevState.options[1] = prevState.options[0];
+			
+					if (this.count > 3)
+					{
+						prevState.options[0] = newOption;
+					}
+					else
+					{
+						this.setState(() => ({ options: [] }));
+					}
+				}
+				else
+				{
+					prevState.options[2] = prevState.options[1];
+					prevState.options[1] = prevState.options[0];
+			
+					if (this.count > 3)
+					{
+						prevState.options[0] = newOption;
+					}
+					else
+					{
+						this.setState(() => ({ options: [] }));
+					}
+				}
+				
+				return {
+						options: prevState.options
+				}})//setState
+			
+				//prevState.options[0] = prevState.options[1];
+				console.log('test2 0 end 1 -0.5');
+				
+				//this.setState((prevState) => ({ 
+				//	options: prevState.options.filter((option) => optionToRemove !== option)
+				//}));
+			}
+			else 
+			{
+				console.log('test2 0 end 1 -1');
+
+				this.setState((prevState) => {
+					var n = prevState.options.indexOf(optionToRemove);
+					console.log('len0 this.count:n is ' + this.count);
+					console.log(n);
+
+					if (n == 0)
+					{
+						if (this.count > 3)
+						{
+							//prevState.options[0] = prevState.options[1];
+							console.log('$len0test2' + this.count);
+				
+						}
+						else if (this.count > 2)
+						{
+							console.log('$len0test2' + this.count);
+						}
+						else if (this.count > 1)
+						{
+											
+							console.log('$len0test2' + this.count);
+											
+						}
+						else if (this.count > 0)
+						{
+							console.log('$len0test20 - start');
+							this.setState(() => ({ options: [] }));
+							//this.setState({ options: prevState.options.splice(0) });
+							console.log('$len0test20 - end');
+
+							//prevState.options[0] = prevState.options[1];
+							//console.log('$len0test21' + this.count);
+							//this.setState({ options: prevState.options.splice(1) });
+							//console.log('$len0test22' + this.count);
+						}
+						else
+						{
+							console.log('$len0test20 - start 2');
+							this.setState(() => ({ options: [] }));
+							//this.setState({ options: prevState.options.splice(0) });
+							console.log('$len0test20 - end 2');
+
+							console.log('$len0test2' + this.count);
+						} 
+					} // n == 0
+					else if (n == 1)
+					{
+						prevState.options[2] = prevState.options[2];
+						prevState.options[1] = prevState.options[0];
+				
+						if (this.count > 3)
+						{
+							prevState.options[0] = newOption;
+						}
+						else
+						{
+							this.setState(() => ({ options: [] }));
+						}
+					}
+					else
+					{
+						prevState.options[2] = prevState.options[1];
+						prevState.options[1] = prevState.options[0];
+				
+						if (this.count > 3)
+						{
+							prevState.options[0] = newOption;
+						}
+						else
+						{
+							this.setState(() => ({ options: [] }));
+						}
+					}
+					
+					return {
+							options: prevState.options
+					}})//setState
+			}
+			
+			console.log('test2 0 end 3');
+		}
+		else 
+		{
+			console.log('len0 0 end else last');
 		}
 		
 	 };
@@ -153,8 +563,6 @@ export default class IndecisionApp extends React.Component {
 
 	render() {
 		const subtitle = 'Put your life in the computer hands';
-
-		
 
 		return (
 			<div>
